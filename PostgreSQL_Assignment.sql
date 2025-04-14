1a44fbf4a9eda8df8d3ff1b92433d70b3463181a
Sakhawat Showrav <sakhawat.showrav@gmail.com>
2025-04-14 23:43:15 +0600

    Insert data into all table

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,6 +1,15 @@
 -- Active: 1744229128545@@127.0.0.1@5432@bookstore_db
 
---
+-- Create database and Connect to the database
+-- CREATE DATABASE bookstore_db;
+-- \c bookstore_db
+
+
+------------------------------------
+--      **** CREATE table ****
+------------------------------------
+
+-- Create books table
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
@@ -10,9 +19,8 @@ CREATE TABLE books (
     published_year INT CHECK(published_year > 0)
 );
 
-DROP Table books;
 
---
+-- Create customers table
 CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -20,6 +28,8 @@ CREATE TABLE customers (
     joined_date DATE DEFAULT CURRENT_DATE
 );
 
+
+-- Create orders table
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     customer_id INT REFERENCES customers(id),
@@ -28,9 +38,12 @@ CREATE TABLE orders (
     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+------------------------------------
+-- **** Insert data into table ****
+------------------------------------
 
 
-
+-- Insert data into books table
 INSERT INTO books (title, author, price, stock, published_year) VALUES
 ('The Pragmatic Programmer', 'Andrew Hunt', 40.00, 10, 1999),
 ('Clean Code', 'Robert C. Martin', 35.00, 15, 2008),
@@ -39,20 +52,30 @@ INSERT INTO books (title, author, price, stock, published_year) VALUES
 ('Refactoring', 'Martin Fowler', 45.00, 12, 1999);
 
 
+-- Insert data into customers table
 INSERT INTO customers (name, email) VALUES
 ('Alice Johnson', 'alice@example.com'),
 ('Bob Smith', 'bob@example.com'),
 ('Charlie Brown', 'charlie@example.com');
 
 
-INSERT INTO orders (customer_id, book_id, quantity) VALUES
-(1, 2, 1),
-(2, 3, 2),
-(3, 1, 1),
-(1, 5, 1),
-(2, 4, 1);
+
+-- Insert data into orders table
+INSERT INTO orders (customer_id, book_id, quantity, order_date) VALUES
+    (1, 2, 1, '2024-03-10'),
+    (2, 1, 1, '2024-02-20'),
+    (1, 3, 2, '2024-03-05');
 
 
+
+
+
+
+
+
+
+
+-- DROP TABLE orders;
 
 SELECT * FROM books;
 
